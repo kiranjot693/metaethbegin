231184627379ddad2020120beded857585fab4f9
Kiran Jot Singh <167282873+kiranjot693@users.noreply.github.com>
2024-05-30 19:16:32 +0530

    Add files via upload

---
+++ hw.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+
+contract MyToken {
+
+    // Public variables to store details about the coin
+    string public tokenName = "MyToken";
+    string public tokenAbbrv = "MTK";
+    uint256 public totalSupply;
+
+    // Mapping of addresses to balances
+    mapping(address => uint256) public balances;
+
+    // Mint function to create new tokens
+    function mint(address _to, uint256 _value) public {
+        totalSupply += _value;
+        balances[_to] += _value;
+    }
+
+    // Burn function to destroy tokens
+    function burn(address _from, uint256 _value) public {
+        require(balances[_from] >= _value, "Insufficient balance to burn");
+        totalSupply -= _value;
+        balances[_from] -= _value;
+    }
+}
